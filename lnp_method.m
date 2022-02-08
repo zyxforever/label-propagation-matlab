5618cd21d2b5fa19e3ac51a6dc4c371234c2bc23
liyaoxing203 <66418939+liyaoxing203@users.noreply.github.com>
2022-02-08 22:22:45 +0800

    Add files via upload

--- lnp_method.m
+++ lnp_method.m
@@ -8,24 +8,33 @@ function [F]=lnp_method(X,k_num,Y_input,n_iter)
     [d,Idx]=sort(dist,2);
     Idx(:,1)=[];
     k_idx=Idx(:,1:k_num);
+    W=zeros(n,n);
     for k=1: n
         G=X(k_idx(k,:),:);
-        H=pdist2(G,G,'euclidean').^2;
-        %f=zeros(k_num,1);
-        %     A=G';
-        %     b=X(k,:)';
+        GG=G';
+        xi=X(k,:)';
+
+        for j=1:k_num
+            for u=1:k_num
+                H(j,u)=(xi-GG(:,j))'*(xi-GG(:,u));
+          
+%             H(j,u)=((xi-GG(:,j))'*(xi-GG(:,j))+(xi-GG(:,u))'*(xi-GG(:,u)))/2;
+            end
+        end
 
         Aeq=ones(1,k_num);
         beq=1;
         lb=zeros(k_num,1);
         %     [x,fval,exitflag,output,lambda] = quadprog(H,f,A,b,[],[],lb);
-        options = optimoptions('quadprog','Display','none');
-        %     [x,fval,exitflag,output,lambda] = quadprog(H,f,A,b,Aeq,beq,lb);
-        [x,fval,exitflag,output,lambda] = quadprog(H,[],[],[],Aeq,beq,lb);
+        options = optimoptions('quadprog','Display','off');
+%             [x,fval,exitflag,output,lambda] = quadprog(H,[],A,b,Aeq,beq,lb);
+%     [x,fval,exitflag,output,lambda] = quadprog(H,[],[],[],Aeq,beq,lb);
+        [x,fval,exitflag,output,lambda] = quadprog(H,[],[],[],Aeq,beq,lb,[],[],options);
         W(k,k_idx(k,:))=x;
     end
+%     size(W)
     S=W;
-    
+
     %propagation
     alpha=0.99;
     F=Y_input;
--- sis_method.m
+++ sis_method.m
@@ -1,6 +1,6 @@
 function [F]=sis_method(X,k,Y_input,n_iter)
 
-    %k=k*1.5;
+
     %Similarity matrix
     [n,m]=size(X);
     dist = pdist2(X,X,'euclidean');
@@ -8,28 +8,31 @@ function [F]=sis_method(X,k,Y_input,n_iter)
     [d,Idx]=sort(dist,2);
     Idx(:,1)=[];
     k_idx=Idx(:,1:k);
+    W=zeros(n,n);
     for i=1: n
-        Q_x= X(k_idx(i),:);
-        xi=X(i,:);
-        Xi=repmat(xi,k,1);
-%         disp(size(Xi));
-%         disp(size(Q_x));
-        C=(Xi-Q_x)*(Xi-Q_x)';
-        C=C+diag(ones(k,1))*(1e-3)*trace(C);
-        C_inv=pinv(C);
-        %tol=sum(C_inv,'all');
-        tol=sum(sum(C_inv));
-        if tol ==0
-            tol=tol+(1e-6);
-        end
-        l=sum(C_inv,2)/tol;
-        l(l<=0)=0;
-        t=sum(l);
-        if t ==0
-            tol=tol+(1e-6);
-        end
-        l=l/t;
+        G=X(k_idx(k,:),:);
+        b=X(i,:);
+        l=l1_ls_nonneg(G',b',1);
         W(i,k_idx(i,:))=l;
+%         Q_x= X(k_idx(i),:);
+%         xi=X(i,:);
+%         Xi=repmat(xi,k,1);
+% 
+%         C=(Xi-Q_x)*(Xi-Q_x)';
+%         C=C+diag(ones(k,1))*(1e-3)*trace(C);
+%         C_inv=pinv(C);
+%         tol=sum(C_inv,'all');
+%         if tol ==0
+%             tol=tol+(1e-6);
+%         end
+%         l=sum(C_inv,2)/tol;
+%         l(l<=0)=0;
+%         t=sum(l);
+%         if t ==0
+%             tol=tol+(1e-6);
+%         end
+%         l=l/t;
+%         W(i,k_idx(i,:))=l;
         
     end
     S=W;
